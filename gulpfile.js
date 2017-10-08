eb836ba30cfa6d89844c824b711551577d7ddd15
李茂峰 <limaofeng@msn.com>
2017-10-08 22:40:55 +0800

    gulpfile 添加 JavaScript 压缩配置

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,41 @@
 // Less configuration
-// yarn add --dev gulp gulp-less gulp-clean-css gulp-rename gulp-if gulp-sourcemaps
+// npm install gulp gulp-less gulp-clean-css gulp-rename gulp-if gulp-sourcemaps gulp-uglify less
 const gulp = require('gulp');
 const less = require('gulp-less');
 const minify = require('gulp-clean-css');
 const rename = require('gulp-rename');
 const gulpIf = require('gulp-if');
 const sourcemaps = require('gulp-sourcemaps');
+const uglify = require('gulp-uglify');
+
+const lessPaths = ['public/assets/css/skins/*.less']
+const jsPaths = ['public/assets/*.js']
 
 gulp.task('less', () => {
-  gulp.src(['public/assets/css/skins/*.less'])
+  gulp.src(lessPaths)
     .pipe(sourcemaps.init())
     .pipe(less())
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest(f => f.base))
     .pipe(gulpIf('*.css', minify({ sourceMap: false })))
-    .pipe(minify())
     .pipe(rename({ suffix: '.min' }))
     .pipe(gulpIf('*.css', gulp.dest(f => f.base)));
 });
 
-gulp.task('default', ['less'], () => {
-  gulp.watch('public/assets/css/skins/*.less', ['less']).on('change', event => {
+gulp.task('script', () => {
+  gulp.src(jsPaths)
+    .pipe(sourcemaps.init())
+    .pipe(uglify())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulpIf((file) => !/\.min\.min/.test(file.path), gulp.dest(f => f.base)))
+});
+
+gulp.task('default', [], () => {
+  gulp.watch(lessPaths, ['less']).on('change', event => {
+    console.log(`File ${event.path} was ${event.type}, running tasks...`);
+  });
+  gulp.watch(jsPaths, ['script']).on('change', event => {
     console.log(`File ${event.path} was ${event.type}, running tasks...`);
   });
 });
