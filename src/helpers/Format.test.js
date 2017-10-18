ddf5bfecaf0e05e306ae7031b36731eb85d18de5
李茂峰 <limaofeng@msn.com>
2017-10-18 13:03:51 +0800

    测试文字修改

--- src/helpers/Format.test.js
+++ src/helpers/Format.test.js
@@ -12,50 +12,50 @@ import {
 } from './Format';
 
 describe('format utils', () => {
-  it('ellipsis', () => {
+  it('test ellipsis', () => {
     expect(ellipsis('出现省略号吧，是不是', 12)).toBe('出现省略号吧...');
   });
 
-  it('lowercase', () => {
+  it('test lowercase', () => {
     expect(lowercase('to LowerCase')).toBe('to lowercase');
   });
 
-  it('string uppercase', () => {
+  it('test uppercase', () => {
     expect(uppercase('to uppercase')).toBe('TO UPPERCASE');
   });
 
-  it('file size', () => {
+  it('test fileSize', () => {
     expect(fileSize(1023)).toBe('1023 bytes');
     expect(fileSize(1024)).toBe('1 KB');
     expect(fileSize(1024 * 1024)).toBe('1 MB');
   });
 
-  it('date', () => {
+  it('test format date', () => {
     const data = new Date('1985-03-04 12:12:12');
     expect(date(data, 'yyyy-MM-dd HH:mm:ss')).toBe('1985-03-04 12:12:12');
   });
 
-  it('number', () => {
+  it('test format number', () => {
     expect(number('1000', '0.00')).toBe('1000.00');
   });
 
-  it('htmlEncode', () => {
+  it('test string htmlEncode', () => {
     expect(htmlEncode('<div><span>Kelly</span> Harber</div>')).toBe(
       '&lt;div&gt;&lt;span&gt;Kelly&lt;/span&gt; Harber&lt;/div&gt;'
     );
   });
 
-  it('htmlDecode', () => {
+  it('test string htmlDecode', () => {
     expect(htmlDecode('&lt;div&gt;&lt;span&gt;Kelly&lt;/span&gt; Harber&lt;/div&gt;')).toBe(
       '<div><span>Kelly</span> Harber</div>'
     );
   });
 
-  it('capitalize', () => {
+  it('test string capitalize', () => {
     expect(capitalize('Alivia Rapid')).toBe('Alivia rapid');
   });
 
-  it('parseQueryString', () => {
+  it('test url parseQueryString', () => {
     expect(parseQueryString('?name=Jayce&adrees=Kiehnton')).toEqual({ adrees: 'Kiehnton', name: 'Jayce' });
   });
 });
